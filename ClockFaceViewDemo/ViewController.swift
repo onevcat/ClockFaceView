eae5e24339d8d04518d923926b1063a4513ec58a
onevcat <onevcat@gmail.com>
2014-10-19 14:11:17 +0900

    Initial Commit

---
+++ ClockFaceViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClockFaceViewDemo
+//
+//  Created by 王 巍 on 14/10/19.
+//  Copyright (c) 2014年 OneV's Den. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
